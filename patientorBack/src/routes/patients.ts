1010cef84195e897113bfcc986346484bab4c060
Yonatan Boehm <yonatanboehm@Yonatans-MacBook-Pro.local>
2024-03-13 14:38:51 +0200

    exercise 9.12

--- patientorBack/src/routes/patients.ts
+++ patientorBack/src/routes/patients.ts
@@ -7,4 +7,9 @@ router.get('/', (_req, res) => {
   res.json(patientService.getNonSensitivePatients());
 });
 
+router.post('/', (req, res) => {
+  const newPatient = req.body
+  res.json(patientService.addPatient(newPatient));
+});
+
 export default router;
--- patientorBack/src/services/patientService.ts
+++ patientorBack/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import patientsData  from "../../data/patients";
-import { NonSensitivePatient, Patient } from "../types";
+import { NonSensitivePatient, Patient, NewPatient } from "../types";
+import { v1 as uuid } from 'uuid'
 
 const getNonSensitivePatients = (): NonSensitivePatient[] => {
   return patientsData.map(({ id, name, dateOfBirth, gender, occupation}) => ({
@@ -15,4 +16,11 @@ const getPatients = (): Patient[] => {
   return patientsData;
 };
 
-export default { getNonSensitivePatients, getPatients };
+const addPatient = (object: NewPatient): Patient => {
+  const id: string = uuid()
+  const newPatient: Patient = {...object, id}
+  patientsData.push(newPatient)
+  return newPatient
+};
+
+export default { getNonSensitivePatients, getPatients, addPatient };
